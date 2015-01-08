07c366124a0d140e40f0df76d7d59cbbe43983a6
Robert Nealan <robertnealan@gmail.com>
2015-01-08 15:25:36 -0800

    Separating theme from Ghost core, bumped version to 0.0.10

---
+++ README.md
@@ -0,0 +1,3 @@
+# Midway
+
+A custom theme designed and built to be used on the blog of Robert Nealan ([robertnealan.com](http://robertnealan.com)), and named after the [Midway Atoll](http://en.wikipedia.org/wiki/Midway_Atoll) in the Pacific Ocean.
---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+	$('.menu-trigger').on('click', function(e) {
+		e.preventDefault();
+		$('body').toggleClass('menu-open');
+	});
+
+	$('.page-wrapper').on('click', function(e) {
+		if($('body').hasClass('menu-open')) {
+			if( !$( e.target ).hasClass( 'menu-trigger' ) ) {
+		        $('body').removeClass('menu-open');
+		    }
+		}
+	});
+});
